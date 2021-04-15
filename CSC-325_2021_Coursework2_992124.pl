dcce5adf08b34eb27025b2beeefc61ea09b3de1b
jkwok678 <jkwok678@gmail.com>
2021-04-15 14:36:43 +0100

    A woman knows him
    Should work

--- CSC-325_2021_Coursework2_992124.pl
+++ CSC-325_2021_Coursework2_992124.pl
@@ -11,12 +11,14 @@
 %vp(vp(V,NP),SP,P) --> v(V,tv,SP,P),np(NP,_).
 %vp(vp(V,PRO),SP,P) --> v(V,tv,SP,P),pro(PRO,SP,_,object).
 %vp(vp(V),SP,P) --> v(V,iv,SP,P).
-
+%
 %nbar(jp(adj(tall), jp(adj(young), n(men))))
 s(s(NP,VP)) --> np(NP,SP),vp(VP,SP).
 np(np(DET,N),SP) --> det(DET,SP),n(N,SP).
-vp(vp(V,NP),SP) --> v(V,tv,SP,P),np(NP,_).
-vp(vp(V),SP) --> v(V,iv,SP).
+vp(vp(V,NP),SP) --> v(V,tv,SP,3),np(NP,_).
+vp(vp(V,PRO),SP) --> v(V,tv,SP,3),pro(PRO,SP,_,object).
+
+vp(vp(V),SP) --> v(V,iv,SP,_).
 
 
 
